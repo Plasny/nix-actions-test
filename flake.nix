fab87e6ed3b11f7c46a3288530ba3b84d9f83582
Plasny <git.plasny.uq95y@slmail.me>
2022-11-01 19:23:59 +0100

    test4

--- flake.nix
+++ flake.nix
@@ -10,17 +10,27 @@
 		flake-utils.lib.eachDefaultSystem (system:
 			let
 				pkgs = nixpkgs.legacyPackages.${system};
-				ci-formatting = pkgs.writeScriptBin "code-formatting" ''
-					echo "clang-format code formatting"
+				ci-code-format = pkgs.writeScriptBin "code-format" ''
+					echo "-> clang-format code formatting"
 					clang-format --style=microsoft -i src/*
 
-					echo "editorconfig code formatting"
+					# echo "-> editorconfig code formatting"
 					# eclint fix *
+				'';
+				ci-code-update = pkgs.writeScriptBin "code-update" ''
+					echo "-> push changed code to git"
+					git config --global user.name 'Plasny'
+					git config --global user.email 'git.plasny.uq95y@slmail.me'
+					git remote set-url origin https://x-access-token:${{ secrets.GITHUB_TOKEN }}@github.com/${{ github.repository }}
+					git commit -am "Auto update for $(git log -n 1 --pretty=format:"%h")"
+					git push
 				'';
 			in {
 				devShells = {
 					default = pkgs.mkShell {
-						buildInputs = [ ci-formatting ] ++ (with pkgs; [
+						buildInputs = [ ci-code-format, ci-code-update ] ++ (with pkgs; [
+							git
+
 							# packages needed for code formatting
 							clang
 							eclint
